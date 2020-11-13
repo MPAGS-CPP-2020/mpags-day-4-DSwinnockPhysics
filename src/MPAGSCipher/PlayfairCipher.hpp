4239056e319fd8b8b4f8f8e18399d1024fe70f6a
root <Daniel.Swinnock@warwick.ac.uk>
2020-11-13 16:14:10 +0000

    Added PlayfairCipher key and map.

---
+++ src/MPAGSCipher/PlayfairCipher.hpp
@@ -0,0 +1,59 @@
+#ifndef MPAGSCIPHER_PLAYFAIRCIPHER_HPP
+#define MPAGSCIPHER_PLAYFAIRCIPHER_HPP
+
+#include <string>
+#include <vector>
+
+#include "CipherMode.hpp"
+
+
+
+/**
+ * \filePlayfairCipher.hpp
+ * \brief Contains the declaration of the PlayfairCipher class
+ */
+
+/**
+ * \class PlayfairCipher
+ * \brief Encrypt or decrypt text using the Playfair cipher with the given key
+ */
+
+class PlayfairCipher {
+  public:
+    /**
+     * Create a new PlayfairCipher with the given key
+     *
+     * \param key the key to use in the cipher
+     */
+    explicit PlayfairCipher( const std::string& key );
+
+    /**
+     * Sets the key (for use in the constructor)
+     * 
+     * \param key the key to use in the cipher
+     */
+    std::string setKey ( const std::string& key );
+
+    /**
+     * Apply the cipher to the provided text
+     *
+     * \param inputText the text to encrypt or decrypt
+     * \param cipherMode whether to encrypt or decrypt the input text
+     * \return the result of applying the cipher to the input text
+     */
+    std::string applyCipher( const std::string& inputText, const CipherMode cipherMode ) const;
+
+  private:
+    /// The alphabet - used to determine the cipher character given the plain character and the key
+    const std::vector<char> alphabet_ = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    /// The size of the alphabet
+    const std::vector<char>::size_type alphabetSize_ = alphabet_.size();
+    
+
+    /// The cipher key
+    std::string key_ = "";
+};
+
+
+#endif
